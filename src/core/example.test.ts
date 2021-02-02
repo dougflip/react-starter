ac9dde2fcb891f40c76a1789590fcd2e3a077190
dougflip <douglasdifilippo@gmail.com>
2021-02-01 21:36:26 -0500

    Adds eslint to the mix

--- src/core/example.test.ts
+++ src/core/example.test.ts
@@ -1,7 +1,17 @@
+interface Person {
+  name: string;
+}
+
 describe("example tests", () => {
   it("respects TS", () => {
     // a type mismatch here will cause the tests to fail to compile
-    const name: string = "Doug";
-    expect(name).toBe("Doug");
+    const person: Person = { name: "Geddy" };
+
+    // verify snapshots are wired up
+    expect(person).toMatchInlineSnapshot(`
+      Object {
+        "name": "Geddy",
+      }
+    `);
   });
 });
