244a8a99ae2c73ce91819cf6ba50783d3e0816d7
dougflip <douglasdifilippo@gmail.com>
2022-04-05 14:08:43 -0400

    Adds more eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
