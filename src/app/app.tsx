c3e220e743f99867c3a84767f0c57a422aa01012
dougflip <douglasdifilippo@gmail.com>
2022-04-27 10:10:12 -0400

    Creates a `renderApp` test helper
    The goal is to have a test helper which can render the entire
    app at a given route. This is useful when you want to test an
    entire page full bootstrapped in the app.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalErrorBoundary } from "~/components/global-error-boundary";
 import { Home } from "~/pages/home";
 
 const queryClient = new QueryClient();
 
+type AppRoutesProps = {
+  renderRoutes?: (children: JSX.Element) => JSX.Element;
+};
+
+/**
+ * Renders the top level routes of the app.
+ * Provides a prop to replace the underlying router implementation.
+ * The actual app will use the BrowserRouter (default value), but tests can
+ * swap this out for a MemoryRouter.
+ */
+export function AppRoutes({
+  renderRoutes = (routes) => <BrowserRouter>{routes}</BrowserRouter>,
+}: AppRoutesProps): JSX.Element {
+  return renderRoutes(
+    <Routes>
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
+
+/**
+ * Renders the App including top level Providers.
+ */
 export function App(): JSX.Element {
   return (
     <React.StrictMode>
       <GlobalErrorBoundary>
         <QueryClientProvider client={queryClient}>
           <main>
-            <Router>
-              <Routes>
-                <Route path="*" element={<Home />} />
-              </Routes>
-            </Router>
+            <AppRoutes />
           </main>
         </QueryClientProvider>
       </GlobalErrorBoundary>
