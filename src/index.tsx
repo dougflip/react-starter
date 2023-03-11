2239f58422052a0410042690e68ae20912b5e494
dougflip <douglasdifilippo@gmail.com>
2023-03-10 20:50:55 -0500

    Update to latest msw
    This includes tweaking the way the app starts up.
    In dev mode, make sure we wait until the mock server is registered.
    I noticed a race condition where the expected "failure" request happened
    before msw was ready.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
+import "./index.css";
+
+import { App } from "./app";
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app";
-import "./index.css";
 
 // https://github.com/msutkowski/msw-ts-vitejs/blob/main/src/main.tsx
 async function loadAndStartMockServer() {
   const { worker } = await import("./mocks/browser");
   worker.start();
+  renderApp();
 }
 
-if (process.env.NODE_ENV === "development") {
-  loadAndStartMockServer();
+function renderApp() {
+  ReactDOM.render(<App />, document.getElementById("root"));
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// in development we first load and start the mock server
+process.env.NODE_ENV === "development" ? loadAndStartMockServer() : renderApp();
